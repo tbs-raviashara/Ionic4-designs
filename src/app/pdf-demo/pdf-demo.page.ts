53076e081f5ea20184a84bc2a7f1e96b0c85bdbc
raviasahara <tbs.raviashara@gmail.com>
2019-12-04 16:32:18 +0530

    Create PDF view table

--- src/app/pdf-demo/pdf-demo.page.ts
+++ src/app/pdf-demo/pdf-demo.page.ts
@@ -72,3 +72,26 @@ export class PdfDEmoPage {
       });
   }
 }
+
+/**
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import * as jspdf from 'jspdf';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular';
+
+  @ViewChild('content') content: ElementRef;
+
+  makePdf() {
+    let doc = new jsPDF();
+    doc.addHTML(this.content.nativeElement, function() {
+       doc.save("obrz.pdf");
+    });
+  }
+}
+ */
