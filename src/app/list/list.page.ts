9d084d1fcd8834bbb64148c46fe3aadedba7fcd7
tbs.raviashara <tbs.raviashara@gmail.com>
2020-04-29 16:28:20 +0530

    Add infinite scroll demo.

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-list',
@@ -7,33 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListPage implements OnInit {
   private selectedItem: any;
-  private icons = [
-    'flask',
-    'wifi',
-    'beer',
-    'football',
-    'basketball',
-    'paper-plane',
-    'american-football',
-    'boat',
-    'bluetooth',
-    'build'
-  ];
-  public items: Array<{ title: string; note: string; icon: string }> = [];
-  constructor() {
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
+
+  public items: any = [];
+  public fullResponse: any = [];
+  public startLoop: number = 0;
+  constructor(public http: HttpClient) {
   }
 
   ngOnInit() {
   }
+
+  ionViewWillEnter() {
+    this.http.get('https://jsonplaceholder.typicode.com/comments').subscribe((response: any) => {
+      this.fullResponse = response;
+      let setLength = response.length >= 10 ? 10 : response.length;
+      for (let i = 0; i < setLength; i++) {
+        this.items.push(response[i]);
+      }
+    });
+  }
+
   // add back when alpha.4 is out
   // navigate(item) {
   //   this.router.navigate(['/list', JSON.stringify(item)]);
   // }
+
+  getProducts(infiniteScroll: any) {
+    this.startLoop += 10;
+    let setLength = this.startLoop + 10;
+    setTimeout(() => {
+      for (let i = this.startLoop; i < setLength; i++) {
+        this.items.push(this.fullResponse[i]);
+      }
+      infiniteScroll.target.complete();
+    }, 1000);
+
+    if (this.items.length == this.fullResponse.length) {
+      infiniteScroll.target.disabled = true;
+    }
+  }
 }
