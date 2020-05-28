1746301941e86aed66b526412a13d7be8259dae2
tbs.raviashara <tbs.raviashara@gmail.com>
2020-05-28 17:12:49 +0530

    read and write file.

--- src/app/native-functionality/native-functionality.page.ts
+++ src/app/native-functionality/native-functionality.page.ts
@@ -39,6 +39,7 @@ export class NativeFunctionalityPage {
   check_File() {
     this.file.checkFile(this.file.externalRootDirectory + this.folderName + '/', this.fileName).then((response: any) => {
       console.log('File already created');
+      this.writeExistingFileFunc();
     }).catch((err: any) => {
       this.create_File();
     });
@@ -47,8 +48,93 @@ export class NativeFunctionalityPage {
   create_File() {
     this.file.createFile(this.file.externalRootDirectory + this.folderName, this.fileName, false).then((createFileRes: any) => {
       console.log('File Created Successfully');
+      this.writeExistingFileFunc();
     }).catch((createFileErr: any) => {
       console.log('File Not Created');
+    });
+  }
+
+  dummyJSON: any = [
+    {
+      "userId": 1,
+      "id": 1,
+      "title": "delectus aut autem",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 2,
+      "title": "quis ut nam facilis et officia qui",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 3,
+      "title": "fugiat veniam minus",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 4,
+      "title": "et porro tempora",
+      "completed": true
+    },
+    {
+      "userId": 1,
+      "id": 5,
+      "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 6,
+      "title": "qui ullam ratione quibusdam voluptatem quia omnis",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 7,
+      "title": "illo expedita consequatur quia in",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 8,
+      "title": "quo adipisci enim quam ut ab",
+      "completed": true
+    },
+    {
+      "userId": 1,
+      "id": 9,
+      "title": "molestiae perspiciatis ipsa",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 10,
+      "title": "illo est ratione doloremque quia maiores aut",
+      "completed": true
+    }];
+  writeExistingFileFunc() {
+    this.file.writeExistingFile(this.file.externalRootDirectory + this.folderName, this.fileName, new Blob([JSON.stringify(this.dummyJSON)], { type: 'text/plain' })).then((success: any) => {
+      console.log('Export successfully');
+      this.readExistingFile(this.file.externalRootDirectory + this.folderName + '/' + this.fileName);
+    }).catch(error => {
+      console.log('Something wrong');
+    });
+  }
+
+  readExistingFile(uri: any) {
+    this.file.resolveLocalFilesystemUrl(uri).then((success: any) => {
+      success.file((file) => {
+        const reader = new FileReader();
+        reader.onloadend = (event: any) => {
+          console.log(event.target.result);
+        };
+        reader.readAsText(file);
+      });
+    }).catch(e => {
+      console.log(e);
     });
   }
 
