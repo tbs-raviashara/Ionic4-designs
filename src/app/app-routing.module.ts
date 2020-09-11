643de34d02c4d3d9b57494bf240c9987a93cb6af
tbs-raviashara <tbs.raviashara@gmail.com>
2020-09-11 17:28:45 +0530

    Resolve bug.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,6 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-
-  { path: 'msg-list', loadChildren: './msg-list/msg-list.module#MsgListPageModule' }
-
   {
     path: '',
     redirectTo: 'login-1',
